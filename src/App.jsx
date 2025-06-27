c17361b6979b7e1ddf38bf5d10c87244a9b905d9
Anandhu <114akb@gmail.com>
2025-06-27 05:45:47 +0530

    new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
 import Home from './components/Home';
